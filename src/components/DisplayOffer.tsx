df6dd1e9b920410946a8b5913d997fe26cf75322
Maciej Mendel <maciej.mendel17@gmail.com>
2022-04-13 14:22:59 +0200

    button

--- src/components/DisplayOffer.tsx
+++ src/components/DisplayOffer.tsx
@@ -19,12 +19,12 @@ const DisplayOffer = () => {
       <Row>
         <Col md={7} lg={8} xl={9}>
           {/* IMAGE BOX */}
-          <Card className="mx-sm-5 mx-md-5 mx-lg-5">
+          <Card className="mx-sm-5 mx-md-5">
             <Card.Body>
               <Carousel variant="dark">
                 <Carousel.Item>
                   <img
-                    className="carousel_item"
+                    className="carousel_item d-block w-100"
                     src={`https://picsum.photos/200/300?random=${
                       Math.random() * 100
                     }`}
@@ -33,7 +33,7 @@ const DisplayOffer = () => {
                 </Carousel.Item>
                 <Carousel.Item>
                   <img
-                    className="carousel_item"
+                    className="carousel_item d-block w-100"
                     src={`https://picsum.photos/200/300?random=${
                       Math.random() * 100
                     }`}
@@ -42,7 +42,7 @@ const DisplayOffer = () => {
                 </Carousel.Item>
                 <Carousel.Item>
                   <img
-                    className="carousel_item"
+                    className="carousel_item d-block w-100"
                     src={`https://picsum.photos/200/300?random=${
                       Math.random() * 100
                     }`}
@@ -51,7 +51,7 @@ const DisplayOffer = () => {
                 </Carousel.Item>
                 <Carousel.Item>
                   <img
-                    className="carousel_item"
+                    className="carousel_item d-block w-100"
                     src={`https://picsum.photos/200/300?random=${
                       Math.random() * 100
                     }`}
@@ -60,7 +60,7 @@ const DisplayOffer = () => {
                 </Carousel.Item>
                 <Carousel.Item>
                   <img
-                    className="carousel_item"
+                    className="carousel_item d-block w-100"
                     src={`https://picsum.photos/200/300?random=${
                       Math.random() * 100
                     }`}
@@ -68,9 +68,10 @@ const DisplayOffer = () => {
                   />
                 </Carousel.Item>
               </Carousel>
-              {/* <Button
+              <Button
                 className="pt-2 my-2 my-sm-0"
                 variant="outline-dark light"
+                style={{ display: "flex", marginLeft: "auto" }}
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -83,7 +84,7 @@ const DisplayOffer = () => {
                   <path d="M0 3.5A1.5 1.5 0 0 1 1.5 2h13A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 12.5v-9zM1.5 3a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-13z" />
                   <path d="M2 4.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1H3v2.5a.5.5 0 0 1-1 0v-3zm12 7a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1 0-1H13V8.5a.5.5 0 0 1 1 0v3z" />
                 </svg>
-              </Button>*/}
+              </Button>
             </Card.Body>
           </Card>
         </Col>
