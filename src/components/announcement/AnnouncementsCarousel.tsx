3b73cf4b5a47c8682b3962bdf7a1259088ef117b
Karolina Pierlak <kp055941@student.ath.edu.pl>
2022-04-15 23:21:21 +0200

    center

--- src/components/announcement/AnnouncementsCarousel.tsx
+++ src/components/announcement/AnnouncementsCarousel.tsx
@@ -6,6 +6,7 @@ import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import AnnouncementCard from "./AnnouncementCard";
 import Posts from "./Posts";
+import { ChildProcess } from "child_process";
 
 var settings = {
   dots: false,
@@ -53,6 +54,7 @@ var settings = {
 interface IAnnouncements {
   announcements: IAnnouncement[];
   loading: boolean;
+  center: boolean;
 }
 
 interface IAnnouncement {
@@ -65,6 +67,7 @@ interface IAnnouncement {
 const AnnouncementsCarousel: FC<IAnnouncements> = ({
   announcements,
   loading,
+  center,
 }) => {
   return (
     <>
@@ -84,9 +87,232 @@ const AnnouncementsCarousel: FC<IAnnouncements> = ({
           </Slider>
         </Row>
       ) : (
-        <Row className="d-flex justify-content-center">
-          <Posts announcements={announcements} loading={loading} />
-        </Row>
+        <>
+          <>
+            {center ? (
+              <>
+                {announcements.length >= 2 ? (
+                  announcements.length >= 3 ? (
+                    announcements.length >= 4 ? (
+                      announcements.length >= 5 ? (
+                        announcements.length === 6 ? (
+                          <>
+                            {/* 6 */}
+                            <Row
+                              className="py-1 px-5 px-sm-4 d-block d-xl-none"
+                              fluid
+                            >
+                              <Slider {...settings}>
+                                {announcements.map((announcement, index) => (
+                                  <Col className="px-1 px-sm-1">
+                                    <AnnouncementCard
+                                      linkA={announcement.link}
+                                      title={announcement.title}
+                                      price={announcement.price}
+                                      city={announcement.city}
+                                    />
+                                  </Col>
+                                ))}
+                              </Slider>
+                            </Row>
+                            <Row className="d-flex justify-content-center d-none d-xl-block">
+                              <Posts
+                                announcements={announcements}
+                                loading={loading}
+                              />
+                            </Row>
+                          </>
+                        ) : (
+                          <>
+                            {/* 4 */}
+                            <Col className="d-none d-sm-block">sm</Col>
+                            <Col className="d-none d-md-block">md</Col>
+                            <Col className="d-none d-lg-block">lg</Col>
+                            <Row
+                              className="py-1 px-5 px-sm-4 d-block d-lg-none"
+                              fluid
+                            >
+                              <Slider {...settings}>
+                                {announcements.map((announcement, index) => (
+                                  <Col className="px-1 px-sm-1">
+                                    <AnnouncementCard
+                                      linkA={announcement.link}
+                                      title={announcement.title}
+                                      price={announcement.price}
+                                      city={announcement.city}
+                                    />
+                                  </Col>
+                                ))}
+                              </Slider>
+                            </Row>
+
+                            <Row className="d-none d-lg-block">
+                              <Row>
+                                {announcements.length === 4 ? (
+                                  <Col xl={2}></Col>
+                                ) : (
+                                  <Col xl={1}></Col>
+                                )}
+                                {announcements.map((announcement, index) => (
+                                  <Col lg={3} xl={2} className="p-1 p-xl-2">
+                                    <AnnouncementCard
+                                      linkA={announcement.link}
+                                      title={announcement.title}
+                                      price={announcement.price}
+                                      city={announcement.city}
+                                    />
+                                  </Col>
+                                ))}
+                              </Row>
+                            </Row>
+                          </>
+                        )
+                      ) : (
+                        <>
+                          {/* 3 */}
+                          <Row
+                            className="py-1 px-5 px-sm-4 d-block d-sm-none"
+                            fluid
+                          >
+                            <Slider {...settings}>
+                              {announcements.map((announcement, index) => (
+                                <Col className="px-1 px-sm-1">
+                                  <AnnouncementCard
+                                    linkA={announcement.link}
+                                    title={announcement.title}
+                                    price={announcement.price}
+                                    city={announcement.city}
+                                  />
+                                </Col>
+                              ))}
+                            </Slider>
+                          </Row>
+                          <Row className="d-flex justify-content-center d-none d-sm-block">
+                            <Row className="mx-sm-1 mx-md-0">
+                              <Col
+                                md={1}
+                                xl={3}
+                                className="mx-md-3 mx-lg-4 mx-xl-1"
+                              ></Col>
+                              {announcements.map((announcement, index) => (
+                                <Col
+                                  sm={4}
+                                  md={3}
+                                  lg={3}
+                                  xl={2}
+                                  className="p-1"
+                                >
+                                  <AnnouncementCard
+                                    linkA={announcement.link}
+                                    title={announcement.title}
+                                    price={announcement.price}
+                                    city={announcement.city}
+                                  />
+                                </Col>
+                              ))}
+                            </Row>
+                          </Row>
+                        </>
+                      )
+                    ) : (
+                      <>
+                        {/* 2 */}
+                        <Row
+                          className="py-1 px-5 px-sm-4 d-block d-sm-none"
+                          fluid
+                        >
+                          <Slider {...settings}>
+                            {announcements.map((announcement, index) => (
+                              <Col className="px-1 px-sm-1">
+                                <AnnouncementCard
+                                  linkA={announcement.link}
+                                  title={announcement.title}
+                                  price={announcement.price}
+                                  city={announcement.city}
+                                />
+                              </Col>
+                            ))}
+                          </Slider>
+                        </Row>
+                        <Row className="d-flex justify-content-center d-none d-sm-block">
+                          <Row>
+                            <Col sm={2} md={3} xl={4} className="mx-1"></Col>
+                            {announcements.map((announcement, index) => (
+                              <Col sm={4} md={3} lg={3} xl={2} className="p-1">
+                                <AnnouncementCard
+                                  linkA={announcement.link}
+                                  title={announcement.title}
+                                  price={announcement.price}
+                                  city={announcement.city}
+                                />
+                              </Col>
+                            ))}
+                          </Row>
+                        </Row>
+                      </>
+                    )
+                  ) : (
+                    <>
+                      {/* 2 */}
+                      <Row className="d-flex justify-content-center">
+                        <Row>
+                          <Col
+                            xs={2}
+                            sm={4}
+                            md={4}
+                            xl={5}
+                            className="mx-md-3 mx-lg-4 mx-xl-0"
+                          ></Col>
+                          {announcements.map((announcement, index) => (
+                            <Col
+                              xs={8}
+                              sm={4}
+                              md={3}
+                              lg={3}
+                              xl={2}
+                              className="p-1"
+                            >
+                              <AnnouncementCard
+                                linkA={announcement.link}
+                                title={announcement.title}
+                                price={announcement.price}
+                                city={announcement.city}
+                              />
+                            </Col>
+                          ))}
+                        </Row>
+                      </Row>
+                    </>
+                  )
+                ) : (
+                  <></>
+                )}
+              </>
+            ) : (
+              <Row className="d-flex justify-content-center">
+                <Posts announcements={announcements} loading={loading} />
+              </Row>
+            )}
+          </>
+          {/* <Row className="py-1 px-5 px-sm-4 d-block d-sm-none" fluid>
+             <Slider {...settings}>
+               {announcements.map((announcement, index) => (
+                 <Col className="px-1 px-sm-1">
+                   <AnnouncementCard
+                     linkA={announcement.link}
+                     title={announcement.title}
+                     price={announcement.price}
+                     city={announcement.city}
+                   />
+                 </Col>
+               ))}
+             </Slider>
+           </Row> */}
+
+          {/* <Row className="d-flex justify-content-center">
+             <Posts announcements={announcements} loading={loading} />
+           </Row> */}
+        </>
       )}
     </>
     // <Row className=" py-1 px-5 px-sm-4" fluid>
