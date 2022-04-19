5fdc176df2411dc4bb33ad62b8c7054dfae6c945
Maciej Mendel <maciej.mendel17@gmail.com>
2022-04-19 13:08:04 +0200

    zmieniona kolejnosc

--- src/components/DisplayOfferComponents/DisplayOffer.tsx
+++ src/components/DisplayOfferComponents/DisplayOffer.tsx
@@ -9,10 +9,10 @@ const DisplayOffer = () => {
   return (
     <div className="offerContainerDisplayOffer">
       <Row className="px-sm-3 px-lg-5">
-        <Col xs={12} lg={8}>
+        <Col xs={{ offset: 12, span: 12, order: 1 }} lg={{ span: 8, order: 1 }}>
           <OfferCarousel />
         </Col>
-        <Col xs={12} lg={4}>
+        <Col xs={{ offset: 12, span: 12, order: 3 }} lg={{ span: 4, order: 2 }}>
           <Col className="pt-lg-0">
             <PersonalInfo />
           </Col>
@@ -20,7 +20,7 @@ const DisplayOffer = () => {
             <FavoriteBox />
           </Col>
         </Col>
-        <Col xs={12} className="pt-lg-4">
+        <Col xs={{ offset: 12, span: 12, order: 2 }} className="pt-lg-4">
           <OfferInfo />
         </Col>
       </Row>
