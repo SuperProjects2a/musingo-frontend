46864182b2e63425f6809d4fe1939012ab551953
Maciej Mendel <93255188+MMaciek17@users.noreply.github.com>
2022-04-28 14:27:15 +0200

    Update DisplayOffer.tsx

--- src/components/DisplayOfferComponents/DisplayOffer.tsx
+++ src/components/DisplayOfferComponents/DisplayOffer.tsx
@@ -8,19 +8,19 @@ import OfferInfo from "./OfferInfo";
 const DisplayOffer = () => {
   return (
     <Container className="mb-5 offerContainerDisplayOffer" fluid>
-      <Row className="px-sm-3 px-lg-5 ">
-        <Col xs={{ offset: 12, span: 12, order: 1 }} lg={{span: 8, order: 1 }}>
+      <Row className="px-sm-3 px-lg-4">
+        <Col xs={{ offset: 12, span: 12, order: 1 }} lg={{span: 8, order: 1 }} className="px-lg-4 ">
           <OfferCarousel />
         </Col>
         <Col xs={{ offset: 12, span: 12, order: 3 }} lg={{span: 4, order: 2 }}>
-          <Col className="pt-2 pt-lg-0 pb-lg-2 px-lg-3">
+          <Col className="pt-2 pt-lg-0  px-lg-3 px-xxl-0">
             <PersonalInfo />
           </Col>
-          <Col className="pt-2 pt-lg-4 px-lg-3">
+          <Col className="pt-2 pt-lg-4 px-lg-3 px-xxl-0">
             <FavoriteBox />
           </Col>
         </Col>
-        <Col xs={{ offset: 12, span: 12, order: 2 }} className="pt-2 pt-lg-4">
+        <Col xs={{ offset: 12, span: 12, order: 2 }} className="pt-2 pt-lg-4 px-lg-4">
           <OfferInfo />
         </Col>
       </Row>
