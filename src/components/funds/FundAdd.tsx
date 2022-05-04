b5c6b9f6b42798fa9b46b4a3804f60449fcdc31a
rtrydev <r.kulka44@gmail.com>
2022-05-04 23:13:16 +0200

    payment method list

--- src/components/funds/FundAdd.tsx
+++ src/components/funds/FundAdd.tsx
@@ -7,6 +7,10 @@ import option3 from '../../images/opt3.png';
 import option4 from '../../images/opt4.png';
 import option5 from '../../images/opt5.png';
 
+import paypalLogo from '../../images/paypal-logo.png';
+import cardLogo from '../../images/card-logo.png';
+import przelewyLogo from '../../images/przelewy24-logo.png';
+
 type FundOption = {
     image: string,
     amount: number
@@ -26,12 +30,18 @@ export const FundAdd = () => {
     return (
         <div>
             <h2 className='py-4'>Dodaj fundusze do swojego portfela:</h2>
-            <div className='d-flex flex-wrap justify-content-center pb-4'>
+            <div className='d-flex flex-wrap justify-content-center'>
                 <FundAddOption image={option1} amount={20}></FundAddOption>
                 <FundAddOption image={option2} amount={50}></FundAddOption>
                 <FundAddOption image={option3} amount={100}></FundAddOption>
                 <FundAddOption image={option4} amount={200}></FundAddOption>
                 <FundAddOption image={option5} amount={500}></FundAddOption>
+            </div>
+            <h4 className='pt-4 pb-3'>Metody płatności:</h4>
+            <div className='d-flex flex-wrap justify-content-center'>
+                <Image height={100} src={paypalLogo}></Image>
+                <Image height={100} src={cardLogo}></Image>
+                <Image height={100} src={przelewyLogo}></Image>
             </div>
         </div>
 
