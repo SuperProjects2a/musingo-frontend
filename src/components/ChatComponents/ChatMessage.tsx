ba736029aff1ce4303694dd56ea02c90bf49559f
Maciej Mendel <maciej.mendel17@gmail.com>
2022-05-11 14:16:25 +0200

    v3

--- src/components/ChatComponents/ChatMessage.tsx
+++ src/components/ChatComponents/ChatMessage.tsx
@@ -1,28 +1,55 @@
 import React from "react";
-import { Container, Form, Card, Col } from "react-bootstrap";
+import {Form, Col } from "react-bootstrap";
 
 const informationsMessage = [
   {
     date: "10.10.2022 15:46",
     text: "jakasadasd asd asd asd d asd asd asd as dasd wiahfgd asd asd asd as dasd wiahfgd asd asd asd as dasd wiahfgd asd asd asd as dasd wiahfgas dasd wiahfghfghfghfghfghfghfghfghfghfghdomosas dasd wiahfghfghfghfghfghfghfghfghfghfghdomosas dasd wiahfghfghfghfghfghfghfghfghfghfghdomosc",
   },
+  {
+    date: "11.11.2022 15:46",
+    text: "jakasadasd asd asd asd d asd asd asd as dasd wiahfgd asd asd asd as dasd wiahfgd asd asd asd as dasd wiahfgd asd asd asd as dasd wiahfgas dasd wiahfghfghfghfghfghfghfghfghfghfghdomosas dasd wiahfghfghfghfghfghfghfghfghfghfghdomosas dasd wiahfghfghfghfghfghfghfghfghfghfghdomosc",
+  },
 ];
 
+let userID:number = 1;
+
 const ChatMessage = () => {
+  if (userID == 0) 
+{
   return (
-    <div className="chatMessage py-3 px-3">
+    <div className="rounded border border-light chatMessageSender py-3 px-3" style={{backgroundColor:"lightgray"}}>
       {informationsMessage.map((informationMessage, index) => (
-        <Form.Text>
+        <Form.Text style={{color: 'black'}}>
           <Col>
             <small>{informationMessage.date}</small>
           </Col>
-          <strong>{informationMessage.text}</strong>
+          <strong >{informationMessage.text}</strong>
 
         </Form.Text>
       ))}
     </div>
     
   );
+}
+else
+{
+  return (
+    <div className="rounded border border-light chatMessageUser py-3 px-3" style={{backgroundColor:"#adbce6"}}>
+      {informationsMessage.map((informationMessage, index) => (
+        <Form.Text style={{color: 'black'}}>
+          <Col>
+            <small>{informationMessage.date}</small>
+          </Col>
+          <strong >{informationMessage.text}</strong>
+
+        </Form.Text>
+      ))}
+    </div>
+    
+  );
+}
+
 };
 
 export default ChatMessage;
