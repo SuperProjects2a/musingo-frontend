2faef62d7ed7d667136a61d5674b405313fc8212
Maksymilian Średniawa <72980576+srednimax@users.noreply.github.com>
2022-05-15 18:12:55 +0200

    removed some option from OfferTab and added message

--- src/components/AdminPanelComponents/OfferTab.tsx
+++ src/components/AdminPanelComponents/OfferTab.tsx
@@ -2,23 +2,28 @@ import React, { useState, useEffect } from "react";
 import { Container, Form, Col, Row, Button } from "react-bootstrap";
 import { Formik } from "formik";
 import * as Yup from "yup";
-import { offerBanUnban, reportedOffers } from "../../services/adminService";
+import { offerBanUnban } from "../../services/adminService";
 
 const OfferTab = () => {
   const addOfferSchema = Yup.object().shape({
-    category: Yup.string()
-      .required("Wybierz powód")
-      .matches(
-        /^[Insults,ViolationsOfMusingoRules,Others]*$/i,
-        "Wybierz powód"
-      ),
-    description: Yup.string().required("To pole jest wymagane"),
+    // category: Yup.string()
+    //   .required("Wybierz powód")
+    //   .matches(
+    //     /^[Insults,ViolationsOfMusingoRules,Others]*$/i,
+    //     "Wybierz powód"
+    //   ),
+    // description: Yup.string().required("To pole jest wymagane"),
     id: Yup.string()
       .required("To pole jest wymagane")
       .matches(/^[1-9][0-9]*$/, "Wprowdź poprawne ID")
       .min(1, "Wprowdź poprawne ID"),
   });
-
+  interface IOffer{
+    id:number;
+    title:string;
+    isBanned:boolean;
+  }
+  const [offer,setOffer] = useState<IOffer|null>(null)
   return (
     <div className="px-1 px-md-2 px-lg-4 ">
       <Container
@@ -27,15 +32,17 @@ const OfferTab = () => {
       >
         <Formik
           initialValues={{
-            category: "",
-            description: "",
+            // category: "",
+            // description: "",
             id: "",
           }}
           validationSchema={addOfferSchema}
           onSubmit={(values, { setSubmitting, resetForm }) => {
             setSubmitting(true);
             offerBanUnban(Number(values.id)).then((result) => {
-              if (result.status === 200) resetForm();
+              if (result.status === 200){ 
+                setOffer(result.data);
+                resetForm();}
             });
             setSubmitting(false);
           }}
@@ -71,8 +78,17 @@ const OfferTab = () => {
                       {errors.id}
                     </Form.Control.Feedback>
                   </Form.Group>
+                  {offer == null || (
+                    <div className=" mt-3">
+                      <p className="text-success" style={{ textAlign: "left" }}>
+                        {offer.isBanned == true
+                          ? `Oferta ${offer.title} o id ${offer.id} została pomyślnie zbanowana`
+                          : `Oferta ${offer.title} o id ${offer.id} została pomyślnie odbanowana`}
+                      </p>
+                    </div>
+                  )}
                 </Col>
-                <Col sm="6">
+                {/* <Col sm="6">
                   <Form.Group className="py-2">
                     <Form.Label className="labelText">
                       <strong>Powód</strong>
@@ -123,7 +139,7 @@ const OfferTab = () => {
                   <Form.Control.Feedback type="invalid">
                     {errors.description}
                   </Form.Control.Feedback>
-                </Form.Group>
+                </Form.Group> */}
               </Form.Group>
               <Row>
                 <Col>
