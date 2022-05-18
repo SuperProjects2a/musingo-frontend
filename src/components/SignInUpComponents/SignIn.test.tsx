04876152e12ce36062cede07377c2567579408f5
Maksymilian Średniawa <72980576+srednimax@users.noreply.github.com>
2022-05-18 18:58:15 +0200

    from should be filled

--- src/components/SignInUpComponents/SignIn.test.tsx
+++ src/components/SignInUpComponents/SignIn.test.tsx
@@ -1,5 +1,5 @@
 import SignIn from "./SignIn";
-import { render } from "@testing-library/react";
+import { render,fireEvent,act } from "@testing-library/react";
 
 describe("<SignIn />", () => {
   test("should display a blank login form", async () => {
@@ -11,4 +11,15 @@ describe("<SignIn />", () => {
     expect(getByPlaceholderText("Wprowadź hasło")).toBeEmptyDOMElement();
     expect(getByPlaceholderText("Wprowadź adres email")).toBeEmptyDOMElement();
   });
+  test("Form should be filled",async () =>{
+    const {getByPlaceholderText} = render(<SignIn />);
+    console.log(getByPlaceholderText("Wprowadź adres email"))
+    await act(async () => {
+        fireEvent.change(getByPlaceholderText("Wprowadź adres email"), {target: {value: 'Gracja65@hotmail.com'}});
+        fireEvent.change(getByPlaceholderText("Wprowadź hasło"), {target: {value: '123$Qwer'}});
+      })
+      console.log(getByPlaceholderText("Wprowadź adres email"))
+       expect(getByPlaceholderText('Wprowadź adres email')).toHaveValue("Gracja65@hotmail.com");
+       expect(getByPlaceholderText('Wprowadź hasło')).toHaveValue("123$Qwer");
+})
 });
