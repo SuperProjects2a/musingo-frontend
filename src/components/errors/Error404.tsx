0af1a4a204832786a709d4cb7519f567ef740827
Karolina Pierlak <kp055941@student.ath.edu.pl>
2022-05-23 14:07:20 +0200

    404-text

--- src/components/errors/Error404.tsx
+++ src/components/errors/Error404.tsx
@@ -1,12 +1,20 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Button } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFaceSadTear } from "@fortawesome/free-regular-svg-icons";
 
 const Error404 = () => {
   return (
-    <Container className="py-5" fluid>
-      <h1>404</h1>
-      <h5>Strona nie została znaleziona</h5>
-      <p>sss</p>
+    <Container className="py-5 my-4" fluid>
+      <FontAwesomeIcon
+        icon={faFaceSadTear}
+        style={{ height: "125px" }}
+        className="pb-2"
+      />
+      {/* <h1>Error 404</h1> */}
+      <h5>Strona, której szukasz, nie istnieje (błąd 404)</h5>
+      <p> Sprawdź wpisany adres strony, lub</p>
+      <Button variant="dark">Wróć na stronę główną</Button>
     </Container>
   );
 };
