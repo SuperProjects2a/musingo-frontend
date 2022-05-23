b69909b98c61d5d0bb97a1bd9ac28e8b233515e4
Karolina Pierlak <kp055941@student.ath.edu.pl>
2022-05-23 13:38:54 +0200

    404

---
+++ src/components/errors/Error404.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const Error404 = () => {
+  return (
+    <Container className="py-5" fluid>
+      <h1>404</h1>
+      <h5>Strona nie została znaleziona</h5>
+      <p>sss</p>
+    </Container>
+  );
+};
+
+export default Error404;
