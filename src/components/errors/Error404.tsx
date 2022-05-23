bb8d2dfd6019b9996ce7fe834bda4faa3736cb2c
Karolina Pierlak <kp055941@student.ath.edu.pl>
2022-05-23 14:22:01 +0200

    button

--- src/components/errors/Error404.tsx
+++ src/components/errors/Error404.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Container, Button } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFaceSadTear } from "@fortawesome/free-regular-svg-icons";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const Error404 = () => {
   return (
@@ -11,10 +12,10 @@ const Error404 = () => {
         style={{ height: "125px" }}
         className="pb-2"
       />
-      {/* <h1>Error 404</h1> */}
       <h5>Strona, której szukasz, nie istnieje (błąd 404)</h5>
       <p> Sprawdź wpisany adres strony, lub</p>
-      <Button variant="dark">Wróć na stronę główną</Button>
+      {/* <Button variant="dark" onClick={() => this?.props.history.push('/')}>Wróć na stronę główną</Button> */}
+      {/* <Button component={Link} variant="dark" >Wróć na stronę główną</Button> */}
     </Container>
   );
 };
