9d879f3a03d016b8f6bb89bc80c4d11a399a0e7f
Karolina Pierlak <kp055941@student.ath.edu.pl>
2022-05-23 14:44:59 +0200

    button-nabigate

--- src/components/errors/Error404.tsx
+++ src/components/errors/Error404.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import { Container, Button } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFaceSadTear } from "@fortawesome/free-regular-svg-icons";
-import { BrowserRouter as Router } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Error404 = () => {
+  const navigate = useNavigate();
   return (
     <Container className="py-5 my-4" fluid>
       <FontAwesomeIcon
@@ -13,9 +14,9 @@ const Error404 = () => {
         className="pb-2"
       />
       <h5>Strona, której szukasz, nie istnieje (błąd 404)</h5>
-      <p> Sprawdź wpisany adres strony, lub</p>
-      {/* <Button variant="dark" onClick={() => this?.props.history.push('/')}>Wróć na stronę główną</Button> */}
-      {/* <Button component={Link} variant="dark" >Wróć na stronę główną</Button> */}
+      <Button variant="dark" onClick={() => navigate("/")} className="my-1">
+        Wróć na stronę główną
+      </Button>
     </Container>
   );
 };
