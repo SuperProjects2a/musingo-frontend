f7abc1ec780af90793e177b01e4e03d949ff6b32
rtrydev <r.kulka44@gmail.com>
2022-05-29 20:30:10 +0200

    navigate after purchase

--- src/components/DisplayOfferComponents/FavoriteBox.tsx
+++ src/components/DisplayOfferComponents/FavoriteBox.tsx
@@ -1,14 +1,25 @@
 import React from "react";
 import { Card, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 import { buyOffer } from "../../services/offerInteractionService";
 
-const FavoriteBox = ({offerId} : {offerId: number}) => {
+const FavoriteBox = ({ offerId }: { offerId: number }) => {
+  const navigate = useNavigate();
+
   return (
     <Card className="py-4 px-4">
       <Button className="" variant="outline-dark light">
         <strong>Dodaj do obserwowanych</strong>
       </Button>
-      <Button className="mt-2" variant="dark" onClick={() => buyOffer({offerId})}>
+      <Button
+        className="mt-2"
+        variant="dark"
+        onClick={() => {
+          buyOffer({ offerId }).then(() => {
+            navigate("/UserProfile/Fundings");
+          });
+        }}
+      >
         <strong>Kup teraz</strong>
       </Button>
     </Card>
--- src/services/offerInteractionService.ts
+++ src/services/offerInteractionService.ts
@@ -1,5 +1,5 @@
 import http from "./HTTPcommon";
 
-export const buyOffer = ({offerId}:{offerId:number}) => {
-    return http({method: 'post', url: `/OfferInteraction/${offerId}/buy`});
-} 
+export const buyOffer = ({ offerId }: { offerId: number }) => {
+  return http({ method: "post", url: `/OfferInteraction/${offerId}/buy` });
+};
